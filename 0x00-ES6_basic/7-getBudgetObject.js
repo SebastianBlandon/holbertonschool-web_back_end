7daed407173daea4b2b941e232953a5136b72005
SebastianBlandon <blandonodz@gmail.com>
2022-11-28 18:08:06 -0500

    [UPDATED] 7-getBudgetObject.js file

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-    let budget = { income, gdp, capita, };
+    let budget = {
+        income,
+        gdp,
+        capita,
+    };
 
     return budget;
 }
